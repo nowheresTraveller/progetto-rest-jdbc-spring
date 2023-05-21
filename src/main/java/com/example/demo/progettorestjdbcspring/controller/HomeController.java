905a0866bbfe9de33e03fe4593b77837bf6dd850
GiovaCode89 <g.giovanni100@gmail.com>
2023-05-21 21:07:25 +0200

    quarta_commit

--- src/main/java/com/example/demo/progettorestjdbcspring/controller/HomeController.java
+++ src/main/java/com/example/demo/progettorestjdbcspring/controller/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Controller
-public class homeController {
+public class HomeController {
 
     @Autowired
     @Qualifier("prodottoGeneralDaoInterface")
